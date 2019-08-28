6468277e7cdaf210a07d5b60c0393a66025b9f65
Administrator <18056033756@163.com>
2019-08-28 11:11:22 +0800

    work

---
+++ src/main/java/com/hthyaq/zybadmin/mapper/DangerOfDicMapper.java
@@ -0,0 +1,16 @@
+package com.hthyaq.zybadmin.mapper;
+
+import com.hthyaq.zybadmin.model.entity.DangerOfDic;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 职业病危害因素分类目录 Mapper 接口
+ * </p>
+ *
+ * @author hk
+ * @since 2019-08-25
+ */
+public interface DangerOfDicMapper extends BaseMapper<DangerOfDic> {
+
+}
---
+++ src/main/java/com/hthyaq/zybadmin/mapper/WorkMapper.java
@@ -0,0 +1,16 @@
+package com.hthyaq.zybadmin.mapper;
+
+import com.hthyaq.zybadmin.model.entity.Work;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 岗位及工种 Mapper 接口
+ * </p>
+ *
+ * @author hk
+ * @since 2019-08-26
+ */
+public interface WorkMapper extends BaseMapper<Work> {
+
+}
